1351f5161dc054863642a10defa4c341f501b159
G25AIT2032 <g25ait2032@iitj.ac.in>
2026-06-12 03:53:53 +0000

    feat(task3): add model_utils.py — DistilBERT config + label maps [G25AIT2032]

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,13 +1,5 @@
+
 MODEL_NAME = "distilbert-base-uncased"
-
-LABEL2ID = {
-    "ham": 0,
-    "spam": 1
-}
-
-ID2LABEL = {
-    0: "ham",
-    1: "spam"
-}
-
+LABEL2ID   = {"ham": 0, "spam": 1}
+ID2LABEL   = {0: "ham", 1: "spam"}
 MAX_LENGTH = 128
